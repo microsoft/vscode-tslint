b3e4553313c939241856ff0278bbf35d612c09e2
Erich Gamma <egamma@microsoft.com>
2015-10-30 16:27:34 +0100

    updated to latest language client API

--- tslint/extension.ts
+++ tslint/extension.ts
@@ -1,23 +1,26 @@
 import * as path from 'path';
-import { window, workspace, commands } from 'vscode';
-import { LanguageClient, ClientOptions, ClientStarter } from 'vscode-languageclient';
+import { workspace, Disposable } from 'vscode';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions } from 'vscode-languageclient';
 
-export function activate() {
+export function activate(subscriptions: Disposable[]) {
 
 	// We need to go one level up since an extension compile the js code into
 	// the output folder.
-	let module = path.join(__dirname, '..', 'server', 'server.js');
+	let serverModulePath = path.join(__dirname, '..', 'server', 'server.js');
 	let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
-	let clientOptions: ClientOptions = {
-		server: {
-			run: { module },
-			debug: { module, options: debugOptions}
-		},
-		syncTextDocument: (textDocument) => (textDocument.languageId === 'typescript' || textDocument.languageId === 'typescriptreact'),
-		configuration: 'tslint',
-		fileWatchers: workspace.createFileSystemWatcher('**/tslint.json')
+	let serverOptions: ServerOptions = {
+		run: { module: serverModulePath },
+		debug: { module: serverModulePath, options: debugOptions}
+	};
+
+	let clientOptions: LanguageClientOptions = {
+		languageSelector: ['typescript', 'typescriptreact'],
+		synchronize: {
+			configurationSection: 'tslint',
+			fileEvents: workspace.createFileSystemWatcher('**/tslint.json')
+		}
 	}
 
-	let client = new LanguageClient('TS Linter', clientOptions);
-	new ClientStarter(client).watchSetting('tslint.enable');
+	let client = new LanguageClient('TS Linter', serverOptions, clientOptions);
+	subscriptions.push(new SettingMonitor(client, 'tslint.enable').start());
 }
