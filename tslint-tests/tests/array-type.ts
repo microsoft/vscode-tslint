33f3ea3ccbc95295df235c177da55b0e20000574
Erich Gamma <egamma@microsoft.com>
2016-11-27 00:58:23 +0100

    Add source snippet

--- tslint-tests/tests/array-type.ts
+++ tslint-tests/tests/array-type.ts
@@ -1,3 +1,9 @@
+// not supported returns multiple replacements
+            // const fix = new Lint.Fix(Rule.metadata.ruleName, [
+            //     this.createReplacement(typeName.getStart(), parens, space + "Array<"),
+            //     // Delete the square brackets and replace with an angle bracket
+            //     this.createReplacement(typeName.getEnd() - parens, node.getEnd() - typeName.getEnd() + parens, ">"),
+            // ]);
 let t: Array<string> = new Array<string>();
 
 console.log(t);
