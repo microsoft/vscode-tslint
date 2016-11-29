ea1c8fe2de2c369c3279836cf514ce72209b9de6
Erich Gamma <egamma@microsoft.com>
2016-11-29 22:26:34 +0100

    update manual tests

--- tslint-tests/tests/no-var-keyword.ts
+++ tslint-tests/tests/no-var-keyword.ts
@@ -1,9 +1,3 @@
-// This autoFix provide several innerReplacements
-// !! not yet supported !!
-//  innerReplacements: 
-//    [ { innerStart: 36, innerLength: 3, innerText: '' },
-//      { innerStart: 36, innerLength: 0, innerText: 'let' } ] }
-
 
 var anakin: string = "jedi";
 
