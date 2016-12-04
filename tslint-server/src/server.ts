5ba7b271edf1bf971f508a7a95e9fa7791fd0a48
Erich Gamma <egamma@microsoft.com>
2016-12-04 20:32:46 +0100

    add comment-format test

--- tslint-server/src/server.ts
+++ tslint-server/src/server.ts
@@ -99,13 +99,45 @@ let quoteFixCreator: FixCreator = (problem: tslint.RuleFailure, document: server
 	const wrongQuote = problem.getFailure()[0];
 	const fixedQuote = wrongQuote === "'" ? '"' : "'";
 	const contents = document.getText().slice(problem.getStartPosition().getPosition() + 1, problem.getEndPosition().getPosition() - 1);
-	let startPosition = convertToServerPosition(problem.getStartPosition());
-	let endPosition = convertToServerPosition(problem.getEndPosition());
 	return {
-		range: [startPosition, endPosition],
+		range: convertProblemPositionsToRange(problem),
 		text: `${fixedQuote}${contents}${fixedQuote}`
 	};
 };
+
+fixes['quotemark'] = quoteFixCreator;
+
+let whiteSpaceFixCreator: FixCreator = (problem: tslint.RuleFailure, document: server.TextDocument): TSLintAutofixEdit => {
+	// error message: 'missing whitespace'
+	if (problem.getFailure() !== 'missing whitespace') {
+		return null;
+	}
+	const contents = document.getText().slice(problem.getStartPosition().getPosition(), problem.getEndPosition().getPosition());
+	return {
+		range: convertProblemPositionsToRange(problem),
+		text: ` ${contents}`
+	};
+};
+
+fixes['whitespace'] = whiteSpaceFixCreator;
+
+let tripleEqualsFixCreator: FixCreator = (problem: tslint.RuleFailure, document: server.TextDocument): TSLintAutofixEdit => {
+	// error message: '== should be ===' or '!= should be !=='
+	let contents = null;
+	if (problem.getFailure() === '== should be ===') {
+		contents = '===';
+	} else if (problem.getFailure() === '!= should be !==') {
+		contents = '!==';
+	} else {
+		return null;
+	}
+	return {
+		range: convertProblemPositionsToRange(problem),
+		text: `${contents}`
+	};
+};
+
+fixes['triple-equals'] = tripleEqualsFixCreator;
 
 function convertToServerPosition(position: tslint.RuleFailurePosition): server.Position {
 	return {
@@ -114,7 +146,11 @@ function convertToServerPosition(position: tslint.RuleFailurePosition): server.P
 	};
 }
 
-fixes['quotemark'] = quoteFixCreator;
+function convertProblemPositionsToRange(problem: tslint.RuleFailure): [server.Position, server.Position] {
+	let startPosition = convertToServerPosition(problem.getStartPosition());
+	let endPosition = convertToServerPosition(problem.getEndPosition());
+	return [startPosition, endPosition];
+}
 
 export function createVscFixForRuleFailure(problem: tslint.RuleFailure, document: server.TextDocument): TSLintAutofixEdit | undefined {
 	let creator = fixes[problem.getRuleName()];
